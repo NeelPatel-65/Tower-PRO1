1a98a424b33a0bc7a6af553d9347247d41afb78c
NeelPatel-65 <63852426+NeelPatel-65@users.noreply.github.com>
2020-07-07 19:19:00 +0530

    Add files via upload

---
+++ tower-1-master/p5.play-boilerplate-master/Block.js
@@ -0,0 +1,19 @@
+class Block extends Baseclass {
+
+    constructor (x,y) {
+    
+        super(x,y,50,50);
+        this.image = loadImage ("sprites/blockk.png");
+        
+        
+}
+
+    display(){
+  
+   super.display();
+
+   }
+
+
+
+}
---
+++ tower-1-master/p5.play-boilerplate-master/Enemy.js
@@ -0,0 +1,24 @@
+class Enemy extends Baseclass {
+
+    constructor (x,y) {
+    
+        super(x,y,50,50);
+
+        
+        
+
+
+    }
+
+    display(){
+  
+   super.display();
+
+  
+
+
+    }
+
+
+
+}
